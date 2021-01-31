a8bede6286eedc80f88829a9da0f7efdf53a4333
zonaib <zonaib@getfieldforce.com>
2021-01-31 13:44:43 +0500

    setting up structure!

---
+++ Hahn.ApplicatonProcess.December2020.Data/DBContext.cs
@@ -0,0 +1,25 @@
+﻿using Hahn.ApplicatonProcess.December2020.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hahn.ApplicatonProcess.December2020.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+           // var options = new DbContextOptionsBuilder<DBContext>()
+           //         .UseInMemoryDatabase(databaseName: "Test")
+           //         .Options;
+        }
+
+        public DbSet<Applicant> Applicants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ApplicantEntityConfiguration());
+        }
+    }
+}
---
+++ Hahn.ApplicatonProcess.December2020.Data/Models/Applicant.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hahn.ApplicatonProcess.December2020.Data.Models
+{
+    public class Applicant
+    {
+        /// <summary>
+        /// Unique Id
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// Applicant Name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Last Name, Surname
+        /// </summary>
+        public string FamilyName { get; set; }
+        /// <summary>
+        /// Address
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Country of Origin
+        /// </summary>
+        public string CountryOfOrigin { get; set; }
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string EmailAdress { get; set; }
+        /// <summary>
+        /// Applicant Age
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// If hired
+        /// </summary>
+        public bool Hired { get; set; } = false;
+    }
+
+    public class ApplicantEntityConfiguration: IEntityTypeConfiguration<Applicant>
+    {
+        public void Configure(EntityTypeBuilder<Applicant> builder)
+        {
+            builder.Property
+        } 
+    }
+}
