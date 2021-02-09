fb3de75a02312172a077d8fd47fc991cf00dea0e
xonaib <zonaibshah@gmail.com>
2021-02-09 13:15:45 +0500

    Country of Origin Rule

--- Hahn.ApplicatonProcess.December2020.Model/Applicant.cs
+++ Hahn.ApplicatonProcess.December2020.Model/Applicant.cs
@@ -1,7 +1,11 @@
 ﻿using FluentValidation;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Hahn.ApplicatonProcess.December2020.Model
 {
@@ -51,15 +55,33 @@ namespace Hahn.ApplicatonProcess.December2020.Model
     {
         public ApplicantValidator()
         {
-            //RuleFor(x => x.ID).NotNull();
-            //RuleFor(x => x.ID).
+            
             RuleFor(x => x.Name).NotNull().MinimumLength(3);
             RuleFor(x => x.FamilyName).NotNull().MinimumLength(5);
-            RuleFor(x => x.Address).NotNull().MinimumLength(10);
-            //RuleFor(x => x.CountryOfOrigin)
+            RuleFor(x => x.Address).NotNull().MinimumLength(10);            
             RuleFor(x => x.EmailAdress).Matches(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)$");
             RuleFor(x => x.Age).ExclusiveBetween(20, 60);
 
+            RuleFor(x => x.CountryOfOrigin).MustAsync(async (country, cancellation) => {
+                bool exists = await ValidateCountry(country);
+                return exists;
+            }).WithMessage("Country must be valid.");
         }
-    }
+
+        public async Task<bool> ValidateCountry(string country)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = string.Format("https://restcountries.eu/rest/v2/name/{0}?fullText=true", country);
+                //Post http callas.
+                HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }    
 }
